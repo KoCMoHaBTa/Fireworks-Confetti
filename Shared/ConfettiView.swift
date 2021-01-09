f9e3783a6919a71e0a7b21dcf882d6e1cca354d7
Milen Halachev <milenski88@gmail.com>
2021-01-09 15:43:38 +0200

    - nested the respective UIViews hosting the emitter logic

--- Shared/ConfettiView.swift
+++ Shared/ConfettiView.swift
@@ -32,160 +32,145 @@ struct ConfettiView: UIViewRepresentable {
     }
 }
 
-class ConfettiPlayer: NSObject {
- 
-    var players: [AVAudioPlayer] = []
+extension ConfettiView {
     
-    func play(file: URL) {
+    class ConfettiUIView: UIView {
         
-    }
-    
-    func playPop() {
-        
-    }
-    
-    func playFalling() {
-        
-    }
-    let popPlayer = try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "confetti-pop", withExtension: "mp3")!)
-}
+        class ConfettiType {
+            let color: UIColor
+            let shape: ConfettiShape
+            let position: ConfettiPosition
 
-class ConfettiUIView: UIView {
-    
-    class ConfettiType {
-        let color: UIColor
-        let shape: ConfettiShape
-        let position: ConfettiPosition
+            init(color: UIColor, shape: ConfettiShape, position: ConfettiPosition) {
+                self.color = color
+                self.shape = shape
+                self.position = position
+            }
+            
+            lazy var image: UIImage = {
+                let imageRect: CGRect = {
+                    switch shape {
+                    case .rectangle:
+                        return CGRect(x: 0, y: 0, width: 20, height: 13)
+                    case .circle:
+                        return CGRect(x: 0, y: 0, width: 10, height: 10)
+                    }
+                }()
 
-        init(color: UIColor, shape: ConfettiShape, position: ConfettiPosition) {
-            self.color = color
-            self.shape = shape
-            self.position = position
-        }
-        
-        lazy var image: UIImage = {
-            let imageRect: CGRect = {
+                UIGraphicsBeginImageContext(imageRect.size)
+                let context = UIGraphicsGetCurrentContext()!
+                context.setFillColor(color.cgColor)
+
                 switch shape {
                 case .rectangle:
-                    return CGRect(x: 0, y: 0, width: 20, height: 13)
+                    context.fill(imageRect)
                 case .circle:
-                    return CGRect(x: 0, y: 0, width: 10, height: 10)
+                    context.fillEllipse(in: imageRect)
                 }
+
+                let image = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                return image!
             }()
+        }
 
-            UIGraphicsBeginImageContext(imageRect.size)
-            let context = UIGraphicsGetCurrentContext()!
-            context.setFillColor(color.cgColor)
+        enum ConfettiShape {
+            case rectangle
+            case circle
+        }
 
-            switch shape {
-            case .rectangle:
-                context.fill(imageRect)
-            case .circle:
-                context.fillEllipse(in: imageRect)
-            }
+        enum ConfettiPosition {
+            case foreground
+            case background
+        }
 
-            let image = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return image!
-        }()
-    }
+        var confettiTypes: [ConfettiType] = {
+            let confettiColors = [
+                (r:149,g:58,b:255), (r:255,g:195,b:41), (r:255,g:101,b:26),
+                (r:123,g:92,b:255), (r:76,g:126,b:255), (r:71,g:192,b:255),
+                (r:255,g:47,b:39), (r:255,g:91,b:134), (r:233,g:122,b:208)
+                ].map { UIColor(red: $0.r / 255.0, green: $0.g / 255.0, blue: $0.b / 255.0, alpha: 1) }
 
-    enum ConfettiShape {
-        case rectangle
-        case circle
-    }
-
-    enum ConfettiPosition {
-        case foreground
-        case background
-    }
-
-    var confettiTypes: [ConfettiType] = {
-        let confettiColors = [
-            (r:149,g:58,b:255), (r:255,g:195,b:41), (r:255,g:101,b:26),
-            (r:123,g:92,b:255), (r:76,g:126,b:255), (r:71,g:192,b:255),
-            (r:255,g:47,b:39), (r:255,g:91,b:134), (r:233,g:122,b:208)
-            ].map { UIColor(red: $0.r / 255.0, green: $0.g / 255.0, blue: $0.b / 255.0, alpha: 1) }
-
-        // For each position x shape x color, construct an image
-        return [ConfettiPosition.foreground, ConfettiPosition.background].flatMap { position in
-            return [ConfettiShape.rectangle, ConfettiShape.circle].flatMap { shape in
-                return confettiColors.map { color in
-                    return ConfettiType(color: color, shape: shape, position: position)
+            // For each position x shape x color, construct an image
+            return [ConfettiPosition.foreground, ConfettiPosition.background].flatMap { position in
+                return [ConfettiShape.rectangle, ConfettiShape.circle].flatMap { shape in
+                    return confettiColors.map { color in
+                        return ConfettiType(color: color, shape: shape, position: position)
+                    }
                 }
             }
+        }()
+        
+        var emitterLayer: CAEmitterLayer?
+        let confettiPopEffect = Bundle.main.url(forResource: "confetti-pop", withExtension: "mp3")!
+        let confettiFallingEffect = Bundle.main.url(forResource: "confetti-falling", withExtension: "mp3")!
+        let player = AudioPlayer()
+        
+        var isStarted: Bool {
+            
+            return self.emitterLayer?.birthRate == 1 && self.emitterLayer?.lifetime == 1
         }
-    }()
-    
-    var emitterLayer: CAEmitterLayer?
-    let confettiPopEffect = Bundle.main.url(forResource: "confetti-pop", withExtension: "mp3")!
-    let confettiFallingEffect = Bundle.main.url(forResource: "confetti-falling", withExtension: "mp3")!
-    let player = AudioPlayer()
-    
-    var isStarted: Bool {
         
-        return self.emitterLayer?.birthRate == 1 && self.emitterLayer?.lifetime == 1
-    }
-    
-    override func layoutSublayers(of layer: CALayer) {
+        override func layoutSublayers(of layer: CALayer) {
+            
+            super.layoutSublayers(of: layer)
+            
+            self.layoutEmitterLayer()
+        }
         
-        super.layoutSublayers(of: layer)
-        
-        self.layoutEmitterLayer()
-    }
-    
-    func layoutEmitterLayer() {
+        func layoutEmitterLayer() {
+         
+            self.emitterLayer?.frame = self.layer.bounds
+            self.emitterLayer?.emitterPosition = .init(x: self.bounds.midX, y: self.bounds.minY-10)
+            self.emitterLayer?.emitterSize = .init(width: self.bounds.size.width, height: 10)
+        }
      
-        self.emitterLayer?.frame = self.layer.bounds
-        self.emitterLayer?.emitterPosition = .init(x: self.bounds.midX, y: self.bounds.minY-10)
-        self.emitterLayer?.emitterSize = .init(width: self.bounds.size.width, height: 10)
-    }
- 
-    func start() {
-        
-        guard !self.isStarted else {
+        func start() {
             
-            return
-        }
-        
-        let confettiCells: [CAEmitterCell] = self.confettiTypes.map { confettiType in
+            guard !self.isStarted else {
+                
+                return
+            }
             
-            let cell = CAEmitterCell()
-            cell.beginTime = 0.1
-            cell.birthRate = 10
-            cell.lifetime = 10
-            cell.contents = confettiType.image.cgImage
-            cell.emissionRange = CGFloat(Double.pi)
-            cell.spin = 4
-            cell.spinRange = 8
-            cell.velocity = 200
-            cell.velocityRange = 50
-            cell.yAcceleration = 150
-            
-            return cell
-        }
+            let confettiCells: [CAEmitterCell] = self.confettiTypes.map { confettiType in
+                
+                let cell = CAEmitterCell()
+                cell.beginTime = 0.1
+                cell.birthRate = 10
+                cell.lifetime = 10
+                cell.contents = confettiType.image.cgImage
+                cell.emissionRange = CGFloat(Double.pi)
+                cell.spin = 4
+                cell.spinRange = 8
+                cell.velocity = 200
+                cell.velocityRange = 50
+                cell.yAcceleration = 150
+                
+                return cell
+            }
 
-        let emitterLayer = CAEmitterLayer()
-        emitterLayer.emitterCells = confettiCells
-        emitterLayer.emitterShape = .rectangle
-        emitterLayer.beginTime = CACurrentMediaTime()
-        
-        self.emitterLayer?.removeFromSuperlayer()
-        self.emitterLayer = emitterLayer
-        self.layer.addSublayer(emitterLayer)
-        self.layoutEmitterLayer()
-        
-        self.player.play(file: self.confettiPopEffect)
-        self.player.play(file: self.confettiFallingEffect) { player in
+            let emitterLayer = CAEmitterLayer()
+            emitterLayer.emitterCells = confettiCells
+            emitterLayer.emitterShape = .rectangle
+            emitterLayer.beginTime = CACurrentMediaTime()
             
-            player.setVolume(0, fadeDuration: 7)
+            self.emitterLayer?.removeFromSuperlayer()
+            self.emitterLayer = emitterLayer
+            self.layer.addSublayer(emitterLayer)
+            self.layoutEmitterLayer()
+            
+            self.player.play(file: self.confettiPopEffect)
+            self.player.play(file: self.confettiFallingEffect) { player in
+                
+                player.setVolume(0, fadeDuration: 7)
+            }
         }
-    }
-    
-    func stop() {
         
-        self.emitterLayer?.birthRate = 0
-        self.emitterLayer?.lifetime = 0
+        func stop() {
+            
+            self.emitterLayer?.birthRate = 0
+            self.emitterLayer?.lifetime = 0
+        }
     }
 }
 
--- Shared/FireworksView.swift
+++ Shared/FireworksView.swift
@@ -32,96 +32,99 @@ struct FireworksView: UIViewRepresentable {
     }
 }
 
-class FireworksUIView: UIView {
+extension FireworksView {
     
-    var numberOfFireworks = 5
-    var emitterLayer: CAEmitterLayer?
-    let explosionEffect = Bundle.main.url(forResource: "fireworks-explosion", withExtension: "mp3")!
-    let player = AudioPlayer()
-    
-    
-    var isStarted: Bool {
+    class FireworksUIView: UIView {
         
-        return self.emitterLayer?.birthRate == 1 && self.emitterLayer?.lifetime == 1
-    }
-    
-    override func layoutSublayers(of layer: CALayer) {
+        var numberOfFireworks = 5
+        var emitterLayer: CAEmitterLayer?
+        let explosionEffect = Bundle.main.url(forResource: "fireworks-explosion", withExtension: "mp3")!
+        let player = AudioPlayer()
         
-        super.layoutSublayers(of: layer)
         
-        self.layoutEmitterLayer()
-    }
-    
-    func layoutEmitterLayer() {
-     
-        self.emitterLayer?.frame = self.layer.bounds
-        self.emitterLayer?.emitterPosition = self.center
-    }
-    
-    func start() {
-        
-        guard !self.isStarted else {
+        var isStarted: Bool {
             
-            return
+            return self.emitterLayer?.birthRate == 1 && self.emitterLayer?.lifetime == 1
         }
         
-        let emitterLayer = CAEmitterLayer()
-        emitterLayer.beginTime = CACurrentMediaTime()
-        emitterLayer.emitterCells = (0..<self.numberOfFireworks).map { index -> CAEmitterCell in
+        override func layoutSublayers(of layer: CALayer) {
             
-            let birthRate: Float = (Float(index + 1) / Float(self.numberOfFireworks)) + 0.5
-            let lifetime: Float = .random(in: 0.75...1.25)
+            super.layoutSublayers(of: layer)
+            
+            self.layoutEmitterLayer()
+        }
+        
+        func layoutEmitterLayer() {
          
-            let locationCell = CAEmitterCell()
-//            locationCell.contents = UIImage(named: "spark")?.cgImage //uncomment and set `scale` to 1 for debuggin purposes in order to track the fireworks trail.
-            locationCell.scale = 0.0
-            locationCell.birthRate = birthRate
-            locationCell.lifetime = lifetime
-            locationCell.color = UIColor.white.cgColor
-            locationCell.redRange = 1
-            locationCell.greenRange = 1
-            locationCell.blueRange = 1
-            locationCell.velocity = 200
-            locationCell.velocityRange = 50
-            locationCell.emissionLongitude = CGFloat.random(in: Range(uncheckedBounds: (lower: -.pi, upper: .pi)))
-            locationCell.emissionRange = .pi
-
-            let fireworkCell = CAEmitterCell()
-            fireworkCell.contents = UIImage(named: "spark")?.cgImage
-            fireworkCell.name = "Firework"
-            fireworkCell.birthRate = 40000.0
-            fireworkCell.lifetime = 2
-            fireworkCell.beginTime = CFTimeInterval(0.9 * lifetime)
-            fireworkCell.duration = CFTimeInterval(0.1 * lifetime)
-            fireworkCell.velocity = .random(in: 50...160)
-            fireworkCell.emissionRange = 360.0 * (.pi / 180.0)
-            fireworkCell.spin = 114.6 * (.pi / 180.0)
-            fireworkCell.scale = 0.1
-            fireworkCell.scaleSpeed = 0.057//0.09
-            fireworkCell.alphaSpeed = -0.6//-0.7
-            fireworkCell.color = UIColor.white.cgColor
-
-            locationCell.emitterCells = [fireworkCell]
-            
-            let explosionTimerInterval: TimeInterval = TimeInterval(1 / locationCell.birthRate)
-            let explosionDelay: TimeInterval = TimeInterval(0.9 * locationCell.lifetime)
-            self.player.schedulePlaying(file: self.explosionEffect, every: explosionTimerInterval, delay: explosionDelay)
-            
-            return locationCell
+            self.emitterLayer?.frame = self.layer.bounds
+            self.emitterLayer?.emitterPosition = self.center
         }
         
-        self.emitterLayer?.removeFromSuperlayer()
-        self.emitterLayer = emitterLayer
-        self.layer.addSublayer(emitterLayer)
-        self.layoutEmitterLayer()
-    }
-    
-    func stop() {
+        func start() {
+            
+            guard !self.isStarted else {
+                
+                return
+            }
+            
+            let emitterLayer = CAEmitterLayer()
+            emitterLayer.beginTime = CACurrentMediaTime()
+            emitterLayer.emitterCells = (0..<self.numberOfFireworks).map { index -> CAEmitterCell in
+                
+                let birthRate: Float = (Float(index + 1) / Float(self.numberOfFireworks)) + 0.5
+                let lifetime: Float = .random(in: 0.75...1.25)
+             
+                let locationCell = CAEmitterCell()
+    //            locationCell.contents = UIImage(named: "spark")?.cgImage //uncomment and set `scale` to 1 for debuggin purposes in order to track the fireworks trail.
+                locationCell.scale = 0.0
+                locationCell.birthRate = birthRate
+                locationCell.lifetime = lifetime
+                locationCell.color = UIColor.white.cgColor
+                locationCell.redRange = 1
+                locationCell.greenRange = 1
+                locationCell.blueRange = 1
+                locationCell.velocity = 200
+                locationCell.velocityRange = 50
+                locationCell.emissionLongitude = CGFloat.random(in: Range(uncheckedBounds: (lower: -.pi, upper: .pi)))
+                locationCell.emissionRange = .pi
+
+                let fireworkCell = CAEmitterCell()
+                fireworkCell.contents = UIImage(named: "spark")?.cgImage
+                fireworkCell.name = "Firework"
+                fireworkCell.birthRate = 40000.0
+                fireworkCell.lifetime = 2
+                fireworkCell.beginTime = CFTimeInterval(0.9 * lifetime)
+                fireworkCell.duration = CFTimeInterval(0.1 * lifetime)
+                fireworkCell.velocity = .random(in: 50...160)
+                fireworkCell.emissionRange = 360.0 * (.pi / 180.0)
+                fireworkCell.spin = 114.6 * (.pi / 180.0)
+                fireworkCell.scale = 0.1
+                fireworkCell.scaleSpeed = 0.057//0.09
+                fireworkCell.alphaSpeed = -0.6//-0.7
+                fireworkCell.color = UIColor.white.cgColor
+
+                locationCell.emitterCells = [fireworkCell]
+                
+                let explosionTimerInterval: TimeInterval = TimeInterval(1 / locationCell.birthRate)
+                let explosionDelay: TimeInterval = TimeInterval(0.9 * locationCell.lifetime)
+                self.player.schedulePlaying(file: self.explosionEffect, every: explosionTimerInterval, delay: explosionDelay)
+                
+                return locationCell
+            }
+            
+            self.emitterLayer?.removeFromSuperlayer()
+            self.emitterLayer = emitterLayer
+            self.layer.addSublayer(emitterLayer)
+            self.layoutEmitterLayer()
+        }
         
-        self.emitterLayer?.birthRate = 0
-        self.emitterLayer?.lifetime = 0
-        
-        self.player.cancelScheduledPlaying()
+        func stop() {
+            
+            self.emitterLayer?.birthRate = 0
+            self.emitterLayer?.lifetime = 0
+            
+            self.player.cancelScheduledPlaying()
+        }
     }
 }
 
